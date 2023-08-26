4f98db7b775a2ef6ea7ced5ea82aebba00886928
lazy-guy-1618 <95616943+lazy-guy-1618@users.noreply.github.com>
2023-08-26 14:48:34 +0530

    Renamed application and small changes

---
+++ Assign3/ass3_21CS10005_21CS30031.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#define KEYWORD 120
+#define IDENTIFIER 121
+#define CONSTANT_INT 122
+#define CONSTANT_FLOAT 123
+#define CONSTANT_CHAR 124
+#define LITERAL 125
+#define PUNCTUATOR 126
+#define SL_COMM_START 127
+#define SL_COMM_END 128
+#define ML_COMM_START 129
+#define ML_COMM_END 130
+
+extern int yylex();
+extern char* yytext;
+
+
+int main() {
+    int token;
+    while(token = yylex()) {
+        switch(token) {
+            case KEYWORD: 
+                printf("<KEYWORD, %d, %s>\n", token, yytext); 
+                break;
+            case IDENTIFIER: 
+                printf("<IDENTIFIER, %d, %s>\n", token, yytext); 
+                break;
+            case CONSTANT_INT: 
+                printf("<CONSTANT_INT, %d, %s>\n", token, yytext); 
+                break;
+            case CONSTANT_FLOAT: 
+                printf("<CONSTANT_FLOAT, %d, %s>\n", token, yytext); 
+                break;
+            case CONSTANT_CHAR: 
+                printf("<CONSTANT_CHAR, %d, %s>\n", token, yytext); 
+                break;
+            case LITERAL: 
+                printf("<LITERAL, %d, %s>\n", token, yytext); 
+                break;
+            case PUNCTUATOR: 
+                printf("<PUNCTUATOR, %d, %s>\n", token, yytext); 
+                break;
+            case ML_COMM_START:
+                break;
+            case ML_COMM_END: 
+                printf("<MULTI_LINE_COMMENT, %d>\n", token);  
+                break;
+            case SL_COMM_START:  
+                break;
+            case SL_COMM_END: 
+                printf("<SINGLE_LINE_COMMENT, %d>\n", token); 
+                break;
+            default:
+                printf("<INVALID_TOKEN, %d, %s>\n", token, yytext);
+                break;
+        }
+    }
+    return 0;
+}
